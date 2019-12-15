a4973227db1c365c40a275daf30c774514830817
bambooes <19334103@qq.com>
2019-12-15 08:06:30 +0800

    -update, add new branch

---
+++ conf.d/phpmyadmin/config.default.inc.php
@@ -0,0 +1,9 @@
+<?php
+$cfg['Servers'] = [
+    1 => [
+        'auth_type' => 'cookie',
+        'host' => '10.10.10.1',
+        'port' => 53306,
+        'verbose' => 'Verbose name 1',
+    ]
+];
---
+++ conf.d/phpmyadmin/config.user.inc.php
@@ -0,0 +1,9 @@
+<?php
+$cfg['Servers'][1]['auth_type'] = 'cookie';
+$cfg['Servers'][1]['host'] = '10.10.10.1';
+$cfg['Servers'][1]['connect_type'] = 'tcp';
+$cfg['Servers'][1]['password'] = '';
+$cfg['Servers'][1]['extension'] = 'mysqli';
+$cfg['Servers'][1]['port'] = '3306';
+$cfg['Servers'][1]['verbose'] = 'Verbose name 1';
+$cfg['Servers'][1]['AllowNoPassword'] = false;
